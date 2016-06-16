408be776bed80f9e78f84e499bdeb206b310c0bb
mon3 <seniut.monika@gmail.com>
2016-06-16 17:30:12 +0200

    Added AlertDialog for wrong data input and prepared functions for communication with DB

--- app/src/main/java/com/example/rafalmaselek/colorfun/ResultsActivity.java
+++ app/src/main/java/com/example/rafalmaselek/colorfun/ResultsActivity.java
@@ -1,6 +1,8 @@
 package com.example.rafalmaselek.colorfun;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +12,9 @@ import android.widget.EditText;
 /**
  * Created by Monia on 15.06.2016.
  */
-public class ResultsActivity extends Activity {
+public class ResultsActivity extends Activity  {
     private String result = "";
+    private String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,56 @@ public class ResultsActivity extends Activity {
     public void exit(View view){
         Intent exitIntent = new Intent(this, MainActivity.class);
         startActivity(exitIntent);
+    }
+
+    public void saveData(View view){
+        String dataToSave = "";
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        username = String.valueOf(nameEditText.getText());
+        if (username.equals("nazwa użytkownika")|| username.equals("")){
+            Log.v("uwaga! ", "niepprawne dane");
+            // dialog wskazujący na niepoprawnie wpisane dane
+            //takich nie możemy wpisać do bazy danych
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setTitle("Uwaga!");
+            builder1.setMessage("Proszę podać poprawne dane!");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+//            builder1.setNegativeButton(
+//                    "No",
+//                    new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int id) {
+//                            dialog.cancel();
+//                        }
+//                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+        else{
+            // zapisujemy dane do bazy danych
+            dataToSave = username +": "+result;
+            Log.v("DB: ", dataToSave);
+
+        }
+
+
+
+
+        // zaimplementować zachowanie danych do BD
+        // dane w postaci: username + result jako string
+
 
     }
+
 
 
 
